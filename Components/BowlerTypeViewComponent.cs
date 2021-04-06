74a1dfb4abae13e09f98a0270d3f1a089d45d0c7
Mhardy99 <71671496+Mhardy99@users.noreply.github.com>
2021-04-05 23:40:42 -0600

    Add project files.

---
+++ Components/BowlerTypeViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReallAssignment10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReallAssignment10.Components
+{
+    public class BowlerTypeViewComponent :ViewComponent
+    {
+        private BowlingLeagueContext context;
+
+        public BowlerTypeViewComponent(BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //returning the view with teamsand ordering the returned info
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
